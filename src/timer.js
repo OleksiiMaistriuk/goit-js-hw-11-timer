a923b6991644788fd6885092172cb03f99b9dc47
OleksiiMaistriuk <65893898+OleksiiMaistriuk@users.noreply.github.com>
2020-12-20 16:41:16 +0100

    добавил разметку динамически

--- src/timer.js
+++ src/timer.js
@@ -1,13 +1,28 @@
-const refs = {
-  days: document.querySelector('[data-value="days"]'),
-  hours: document.querySelector('[data-value="hours"]'),
-  mins: document.querySelector('[data-value="mins"]'),
-  secs: document.querySelector('[data-value="secs"]'),
-};
+const template = () => `<div class="field">
+      <span class="value" data-value="days">11</span>
+      <span class="label">Days</span>
+    </div>
+
+    <div class="field">
+      <span class="value" data-value="hours">11</span>
+      <span class="label">Hours</span>
+    </div>
+
+    <div class="field">
+      <span class="value" data-value="mins">11</span>
+      <span class="label">Minutes</span>
+    </div>
+
+    <div class="field">
+      <span class="value" data-value="secs">11</span>
+      <span class="label">Seconds</span>
+    </div>`;
 
 class CountdownTimer {
-  constructor({ targetDate }) {
+  constructor({ targetDate, selector }) {
+    this.container = document.querySelector(selector);
     this.targetDate = targetDate.getTime();
+    this.container.insertAdjacentHTML('beforeend', template());
   }
   start() {
     updateTimer(0);
@@ -20,6 +35,12 @@ const timer = new CountdownTimer({
   selector: '#timer-1',
   targetDate: new Date('Jan 01, 2100'),
 });
+const refs = {
+  days: document.querySelector('[data-value="days"]'),
+  hours: document.querySelector('[data-value="hours"]'),
+  mins: document.querySelector('[data-value="mins"]'),
+  secs: document.querySelector('[data-value="secs"]'),
+};
 
 timer.start();
 
